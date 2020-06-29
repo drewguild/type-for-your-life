e885f4c0cf200472f4b9a479a2c696cbb54cd3ee
drewguild <drew.guild@gmail.com>
2020-06-29 10:37:00 -0400

    Updates

---
+++ precache-manifest.d810248509e19435008faf28c1ca6ad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915da234c0691e5bf1c8401b412bdaa4",
+    "url": "/type-for-your-life/index.html"
+  },
+  {
+    "revision": "62c12a0cfd3052451abf",
+    "url": "/type-for-your-life/static/css/main.b31d2339.chunk.css"
+  },
+  {
+    "revision": "385090f98b172f5ab3fa",
+    "url": "/type-for-your-life/static/js/2.7b5965eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/type-for-your-life/static/js/2.7b5965eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c12a0cfd3052451abf",
+    "url": "/type-for-your-life/static/js/main.71285530.chunk.js"
+  },
+  {
+    "revision": "214873dae968a8c7cad6",
+    "url": "/type-for-your-life/static/js/runtime-main.36a2d067.js"
+  }
+]);
